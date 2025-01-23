64d5af950a7739104d50f2bfcb43adfab014953d
uminchegject <81868146+uminchegject@users.noreply.github.com>
2025-01-23 10:11:08 +0900

    delete build_file

--- build_debug/build/MaterialX-1.38.10/source/MaterialXFormat/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/USD/build_debug/src/MaterialX-1.38.10/source/MaterialXFormat
-# Build directory: D:/USD/build_debug/build/MaterialX-1.38.10/source/MaterialXFormat
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_debug/build/zlib-1.2.13/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/USD/build_debug/src/zlib-1.2.13
-# Build directory: D:/USD/build_debug/build/zlib-1.2.13
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
